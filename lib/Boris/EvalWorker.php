28466ef89239f7f96b844e49d6096d81a0c4e3d7
Alex Walker <alex.walker@answers.com>
2014-09-05 18:58:38 -0500

    imported changes from joddie, making sense of and playing with

--- lib/Boris/EvalWorker.php
+++ lib/Boris/EvalWorker.php
@@ -9,10 +9,15 @@ namespace Boris;
  */
 class EvalWorker {
   const ABNORMAL_EXIT = 255;
-  const DONE   = "\0";
-  const EXITED = "\1";
-  const FAILED = "\2";
-  const READY  = "\3";
+  const DONE     = "\0";
+  const EXITED   = "\1";
+  const FAILED   = "\2";
+  const READY    = "\3";
+  const RESPONSE = "\4";
+
+  /* Request opcodes sent by the ReadlineClient to EvalWorker */
+  const EVALUATE = "\0";
+  const COMPLETE = "\1";
 
   private $_socket;
   private $_exports = array();
@@ -146,65 +151,143 @@ class EvalWorker {
 
       $this->_cancelled = false;
 
-      $__input = $this->_transform($this->_read($this->_socket));
-
-      if ($__input === null) {
+      if (null === ($__input = $this->_read($this->_socket))) {
         continue;
       }
 
-      $__response = self::DONE;
+      /* Dispatch on opcode in first byte */
+      if ($__input[0] === self::EVALUATE) {
+        /* Expression evaluation */
+        $__input = substr($__input, 1);
+        $__response = self::DONE;
 
-      $this->_ppid = posix_getpid();
-      $this->_pid  = pcntl_fork();
+        $this->_ppid = posix_getpid();
+        $this->_pid  = pcntl_fork();
 
-      if ($this->_pid < 0) {
-        throw new \RuntimeException('Failed to fork child labourer');
-      } elseif ($this->_pid > 0) {
-        // kill the child on ctrl-c
-        pcntl_signal(SIGINT, array($this, 'cancelOperation'), true);
-        pcntl_waitpid($this->_pid, $__status);
+        if ($this->_pid < 0) {
+          throw new \RuntimeException('Failed to fork child labourer');
+        } elseif ($this->_pid > 0) {
+          // kill the child on ctrl-c
+          pcntl_signal(SIGINT, array($this, 'cancelOperation'), true);
+          pcntl_waitpid($this->_pid, $__status);
 
-        if (!$this->_cancelled && $__status != (self::ABNORMAL_EXIT << 8)) {
-          $__response = self::EXITED;
+          if (!$this->_cancelled && $__status != (self::ABNORMAL_EXIT << 8)) {
+            $__response = self::EXITED;
+          } else {
+            $this->_runHooks($this->_failureHooks);
+            $__response = self::FAILED;
+          }
         } else {
-          $this->_runHooks($this->_failureHooks);
-          $__response = self::FAILED;
+          // user exception handlers normally cause a clean exit, so Boris will exit too
+          if (!$this->_exceptionHandler =
+            set_exception_handler(array($this, 'delegateExceptionHandler'))) {
+            restore_exception_handler();
+          }
+
+          // undo ctrl-c signal handling ready for user code execution
+          pcntl_signal(SIGINT, SIG_DFL, true);
+          $__pid = posix_getpid();
+
+          $__input = $this->_transform($__input);
+          $__result = eval($__input);
+
+          while (is_string($__result) && stripos($__result, 'return ') === 0) {
+            $__result = eval($__result);
+          }
+
+          if (posix_getpid() != $__pid) {
+            // whatever the user entered caused a forked child
+            // (totally valid, but we don't want that child to loop and wait for input)
+            exit(0);
+          }
+
+          if (preg_match('/\s*return\b/i', $__input)) {
+            fwrite(STDOUT, sprintf("%s\n", $this->_inspector->inspect($__result)));
+          }
+          $this->_expungeOldWorker();
         }
+      } else if ($__input[0] === self::COMPLETE) {
+        $__input = substr($__input, 1);
+        if (preg_match(
+            "/(->|::[$]?)([[:alpha:]_]*[[:alnum:]_]*)$/",
+            $__input,
+            $__match_data,
+            PREG_OFFSET_CAPTURE)) {
+          /* Complete one of the following, depending on the
+           * dereferencing operator:
+           *
+           * an object property or method '->xyz_...',
+           * an static method or constant '::xyz...',
+           * or a static property '::$xyz...'
+           */
+          $__operator = $__match_data[1][0];
+          $__symbol   = $__match_data[2][0];
+          list($__completion_base, $__is_bare) = $this->_getCompletionBase($__input, $__match_data[0][1]);
+          if ($__is_bare) {
+            $__result = $__completion_base;
+          } else {
+            $__result = eval("return $__completion_base;");
+          }
+          switch ($__operator) {
+            case '->':  $__completions = $this->_objectMembers($__result); break;
+            case '::':  $__completions = $this->_staticMembers($__result); break;
+            case '::$': $__completions = $this->_staticProperties($__result); break;
+            default: /* Should never get here */
+              throw new \RuntimeException(sprintf("Unknown operator '%s'", $__operator));
+          }
+          $__completions = $this->_filterCompletions($__completions, $__symbol);
+        } else if (preg_match('/[$]([[:alpha:]_]*[[:alnum:]_]*)$/',
+                              $__input, $__match_data)) {
+          /* Complete a variable name */
+          $__completions = $this->_filterCompletions(array_keys(get_defined_vars()),
+                                                     $__match_data[1]);
+        } else if (preg_match("/\\[(['\"]?)([^\]]*)$/",
+                              $__input, $__match_data, PREG_OFFSET_CAPTURE)) {
+          /* Complete array (hash) index */
+          $__quote  = $__match_data[1][0];
+          $__symbol = $__match_data[2][0];
+          list($__completion_base, $__is_bare) = $this->_getCompletionBase(
+                                                    $__input,
+                                                    $__match_data[0][1]);
+          if ($__is_bare) {
+            $__result = NULL;
+            $__completions = array();
+          } else {
+            $__result = eval("return $__completion_base;");
+            #file_put_contents('/tmp/dbg', var_export(get_defined_vars(),true), FILE_APPEND);
+            $__completions = $this->_filterCompletions(array_keys($__result),
+                                                        $__symbol);
+          }
+          if (! $__quote) {
+            foreach ($__completions as $__k => $__str) {
+              $__completions[$__k] = sprintf("'%s']", str_replace("'", "\\'", trim($__str)));
+            }
+          }
+        } else if (preg_match("/[[:alpha:]_][[:alnum:]_]*$/",
+                              $__input, $__match_data)) {
+          /* Complete function, class or defined constant */
+          $__completions = $this->_filterCompletions($this->_bareSymbols(), $__match_data[0]);
+        } else {
+          $__completions = array();
+        }
+
+        $__serialized = json_encode($__completions);
+        $__response   = self::RESPONSE
+          . pack('N', strlen($__serialized))
+          . $__serialized;
+
+        unset($__serialized, $__completions);
       } else {
-        // user exception handlers normally cause a clean exit, so Boris will exit too
-        if (!$this->_exceptionHandler =
-          set_exception_handler(array($this, 'delegateExceptionHandler'))) {
-          restore_exception_handler();
-        }
-
-        // undo ctrl-c signal handling ready for user code execution
-        pcntl_signal(SIGINT, SIG_DFL, true);
-        $__pid = posix_getpid();
-
-        $__result = eval($__input);
-
-        while (is_string($__result) && stripos($__result, 'return ') === 0) {
-          $__result = eval($__result);
-        }
-
-        if (posix_getpid() != $__pid) {
-          // whatever the user entered caused a forked child
-          // (totally valid, but we don't want that child to loop and wait for input)
-          exit(0);
-        }
-
-        if (preg_match('/\s*return\b/i', $__input)) {
-          fwrite(STDOUT, sprintf("%s\n", $this->_inspector->inspect($__result)));
-        }
-        $this->_expungeOldWorker();
-      }
+        throw new \RuntimeException(sprintf("Bad request code 0x%x", $__input[0]));
+        $__response = self::DONE;
+      } // eo input type handling
 
       $this->_write($this->_socket, $__response);
 
       if ($__response == self::EXITED) {
         exit(0);
       }
-    }
+    } // eo for
   }
 
   /**
@@ -227,10 +310,10 @@ class EvalWorker {
 
   // -- Private Methods
 
-  private function _runHooks($hooks) {
+  private function _runHooks($__hooks) {
     extract($this->_exports);
 
-    foreach ($hooks as $__hook) {
+    foreach ($__hooks as $__hook) {
       if (is_string($__hook)) {
         eval($__hook);
       } elseif (is_callable($__hook)) {
@@ -257,13 +340,18 @@ class EvalWorker {
   }
 
   private function _write($socket, $data) {
-    if (!fwrite($socket, $data)) {
-      throw new \RuntimeException('Socket error: failed to write data');
+    $total = strlen($data);
+    for ($written = 0; $written < $total; $written += $fwrite) {
+      $fwrite = fwrite($socket, substr($data, $written));
+      if ($fwrite === false) {
+        throw new \RuntimeException(
+          sprintf('Socket error: wrote only %d of %d bytes.',
+                  $written, $total));
+      }
     }
   }
 
-  private function _read($socket)
-  {
+  private function _read($socket) {
     $read   = array($socket);
     $except = array($socket);
 
@@ -277,8 +365,12 @@ class EvalWorker {
   }
 
   private function _select(&$read, &$except) {
+    static $alwaysTrue = null;
+    if ($alwaysTrue === null) {
+      $alwaysTrue = function () { return true; };
+    }
     $write = null;
-    set_error_handler(function(){return true;}, E_WARNING);
+    set_error_handler($alwaysTrue, E_WARNING);
     $result = stream_select($read, $write, $except, 10);
     restore_error_handler();
     return $result;
@@ -298,5 +390,148 @@ class EvalWorker {
     }
 
     return $input;
+  }
+
+  /**
+   * Get the portion of code which must be evaluated to perform
+   * tab-completion.
+   *
+   * Example: if the cursor is at the end of the following line:
+   *   printf("%d\n", $arr['index'][123]->member->x_
+   * then the portion of the line which needs evaluation is
+   *   $arr['index'][123]->member
+   *
+   * Other completion code (main loop) determines the incomplete
+   * portion at the end of the line ("->x_" in the example above).
+   * This function works by scanning backward from the beginning of
+   * that fragment, accepting anything that looks like either a
+   * constant array index or a property lookup.
+   */
+  private function _getCompletionBase ($line, $end) {
+    static $ident = "[a-zA-Z_][a-zA-Z0-9_]*";
+    static $number = "\\d+";
+    static $string1 = '"(?:[^"\\\\]|\\\\.)*"';
+    static $string2 = "'(?:[^'\\\\]|\\\\.)*'";
+    static $constant = null;
+    if ($constant === null) {
+      $constant = "(?:{$number}|{$string1}|{$string2})";
+    }
+
+    $start = $end;
+    while (true) {
+      $substr = substr($line, 0, $start);
+      if (preg_match('/(->[$]?|::[$])' . $ident . '$/', $substr, $match)
+         || preg_match('/\[' . $constant . '\]$/', $substr, $match)) {
+        /* Matched either:
+         * - an array index: ['str'], ["str"] or [123]
+         * - property access: ->member or ->$member_name
+         * - or a static property access ::$member */
+        $start -= strlen($match[0]);
+      } else if (preg_match('/[$]' . $ident . '$/', $substr, $match)) {
+        /* Matched a variable $var, which should be the beginning of
+         * the expression. */
+        $start -= strlen($match[0]);
+        return array(substr($line, $start, $end - $start), FALSE);
+      } else if (preg_match("/\\\\?{$ident}(?:\\\\{$ident})*$/", $substr, $match)) {
+        $is_bare = $start == $end;
+        $start -= strlen($match[0]);
+        return array(substr($line, $start, $end - $start), $is_bare);
+      } else {
+        /* Something else: assume we don't want to evaluate it for
+         * tab-completion.
+         */
+        return FALSE;
+      }
+    }
+  }
+
+  /**
+   * Filter possible completions by prefix
+   */
+  private function _filterCompletions($candidates, $prefix) {
+    if (empty($prefix)) {
+      return $candidates;
+    }
+    $completions = array();
+    foreach ($candidates as $candidate) {
+      if (strpos($candidate, $prefix) === 0) {
+        $completions[] = $candidate .'\']';
+      }
+    }
+    return $completions;
+  }
+
+  /**
+   * Return the names of all defined constants, classes, interfaces
+   * and functions.
+   */
+  private function _bareSymbols() {
+    $constants  = array_keys(get_defined_constants());
+    $classes    = get_declared_classes();
+    $interfaces = get_declared_interfaces();
+    $functions  = array();
+    foreach(get_defined_functions() as $type => $names) {
+      foreach($names as $name) {
+        $functions[] = $name . "(";
+      }
+    }
+    return array_merge($constants, $classes, $interfaces, $functions);
+  }
+
+  /**
+   * Return all properties and methods of an object.
+   *
+   * These are the symbols which can appear after the -> operator.
+   */
+  private function _objectMembers($obj) {
+    try {
+      $refl = new \ReflectionObject($obj);
+      $methods = $refl->getMethods(\ReflectionMethod::IS_PUBLIC);
+      foreach ($methods as $method) {
+        $return[] = $method->name . '(';
+      }
+
+      $properties = $refl->getProperties(\ReflectionProperty::IS_PUBLIC);
+      foreach ($properties as $property) {
+        $return[] = $property->name;
+      }
+
+      return $return;
+    } catch(\ReflectionException $e) {
+      return array();
+    }
+  }
+
+  /**
+   * Return the static methods and constants of an object.
+   * 
+   * These are the symbols which can appear after the :: operator
+   */
+  private function _staticMembers($obj) {
+    try {
+      $refl = new \ReflectionClass($obj);
+      $constants = array_keys($refl->getConstants());
+      $methods = array();
+      foreach ($refl->getMethods(\ReflectionMethod::IS_STATIC) as $method) {
+        $methods[] = $method->name . '(';
+      }
+      return array_merge($methods, $constants);
+    } catch(\ReflectionException $e) {
+      return array();
+    }
+  }
+
+  /**
+   * Return static variables for an object
+   *
+   * These appear after the :: operator, preceded by a $ sigil.
+   */
+  private function _staticProperties($obj) {
+    try {
+      $refl = new \ReflectionClass($obj);
+      return array_keys($refl->getStaticProperties());
+    } catch(\ReflectionException $e) {
+      return array();
+    }
   }
 }
--- lib/Boris/ReadlineClient.php
+++ lib/Boris/ReadlineClient.php
@@ -34,6 +34,7 @@ class ReadlineClient {
    */
   public function start($prompt, $historyFile) {
     readline_read_history($historyFile);
+    readline_completion_function(array($this, 'completion_function'));
 
     declare(ticks = 1);
     pcntl_signal(SIGCHLD, SIG_IGN);
@@ -73,14 +74,16 @@ class ReadlineClient {
         readline_add_history($line);
       }
 
-      $buf .= sprintf("%s\n", $line);
+      $buf .= "$line\n";
 
       if ($statements = $parser->statements($buf)) {
         ++$lineno;
 
+        #file_put_contents('/tmp/dbg', var_export(['rline' => $line, 'statements' => $statements], true).PHP_EOL, FILE_APPEND);
         $buf = '';
         foreach ($statements as $stmt) {
-          if (false === $written = fwrite($this->_socket, $stmt)) {
+          if (false === ($written = fwrite($this->_socket,
+                                            EvalWorker::EVALUATE . $stmt))) {
             throw new \RuntimeException('Socket error: failed to write data');
           }
 
@@ -105,5 +108,85 @@ class ReadlineClient {
   public function clear() {
     // FIXME: I'd love to have this send \r to readline so it puts the user on a blank line
     $this->_clear = true;
+  }
+
+  /**
+   * Callback to perform readline completion.
+   *
+   * Sends a message over the socket to the EvalWorker requesting a
+   * list of completions, in order to complete on functions &
+   * variables in the REPL scope.
+   */
+  public function completion_function($word) {
+    $rl_info = readline_info();
+    $line    = substr($rl_info['line_buffer'], 0, $rl_info['point']);
+
+    /* HACK. Ugh. */
+    if (false !== ($pos = strpos($word, '['))) {
+      $prefix = substr($word, 0, $pos + 1);
+    } else if (false !== ($pos = strpos($word, '::'))) {
+      $prefix = substr($word, 0, $pos + 2);
+    #} else if (false !== ($pos = strpos($word, '\\'))) {
+    #  $prefix =
+    #} else if (0 === stripos($line, 'use')) {
+    #  $prefi =
+    } else {
+      $prefix = '';
+    }
+
+    #print("\nbuffer={$rl_info['line_buffer']}\ncompleting=$word\nprefix=$prefix\n");
+    /* Call the EvalWorker to perform completion */
+    #$send = json_encode(
+    $this->_write($this->_socket, EvalWorker::COMPLETE . $line);
+    $completions = $this->_read_unserialize();
+
+    #file_put_contents('/tmp/dbg', var_export(get_defined_vars(), true).PHP_EOL, FILE_APPEND);
+    /* HACK */
+    if ($prefix) {
+      $completions = array_map(
+        function ($str) use ($prefix) {
+          return $prefix . $str;
+        },
+        $completions
+      );
+    }
+    return $completions;
+  }
+
+  /* TODO: refactor me */
+  private function _write($socket, $data) {
+    $total = strlen($data);
+    for ($written = 0; $written < $total; $written += $wrote) {
+      $wrote = fwrite($socket, substr($data, $written));
+      if ($wrote === false) {
+        throw new \RuntimeException(
+          sprintf('Socket error: wrote only %d of %d bytes.',
+                  $written, $total));
+      }
+    }
+    return $written;
+  }
+
+  private function _read($socket, $bytes) {
+    for ($read = ''; strlen($read) < $bytes; $read .= $fread) {
+      $fread = fread($socket, $bytes - strlen($read));
+    }
+    return $read;
+  }
+
+  private function _read_unserialize() {
+    /* Get response: expected to be one-byte opcode,
+     * EvalWorker::RESPONSE, four bytes giving length of message,
+     * and serialized data */
+    $status = $this->_read($this->_socket, 1);
+    if ($status !== EvalWorker::RESPONSE) {
+      throw new \RuntimeException(sprintf('Bad response: 0x%x',
+                                          ord($status)));
+    }
+    $length_packed   = $this->_read($this->_socket, 4);
+    $length_unpacked = unpack('N', $length_packed);
+    $length          = $length_unpacked[1];
+    $serialized      = $this->_read($this->_socket, $length);
+    return json_decode($serialized, true);
   }
 }
